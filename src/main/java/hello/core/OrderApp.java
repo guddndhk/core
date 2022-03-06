2021f26fe840a5d16e9a7484a020dbd675c9b213
guddndhk <guddndhk@naver.com>
2022-03-06 23:12:20 +0900

    22/03/06 spring core study. no coding no sexy do it everything from ur self. ur time still on the load not gonna happen to stop 타입 조회시 같은 타입이 둘 이상 이면 오류 발생 , 빈 이름 으로 지정하여 찾기 , 특정 타입 모두 조회 하기 등 테스트 작성.

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class OrderApp {
     public static void main(String[] args) {
-//
+
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
 //        OrderService orderService = appConfig.orderService();
