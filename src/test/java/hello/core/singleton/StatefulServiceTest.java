779840ee9c2be157cf055e81cb4a504bbcd5ac9e
guddndhk <guddndhk@naver.com>
2022-03-10 23:21:35 +0900

    22/03/10 spring core study. no coding no sexy do it everything from ur self. ur time still on the load not gonna happen to stop 싱글톤 방식의 주의점 코딩 실무에서도 가끔 발생한다고 한다. 싱글톤 패턴은 객체 인스턴스를 하나만 생성해서 공유하는 방식인데 여러 클라이언트가 하나의 같은 객체 인스턴스를 공유하기 때문에 싱글톤 객체는 상태를 stateful 가되면 안된다. 즉 stateless 상태로 설계가 진행되어야 하는것! 특정 클라이언트에 의존적 필드 혹은 값을 변경할 수 있는 필드가 있으면 안됨.

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,41 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA: A 사용자 10000원 주문
+        int userA = statefulService1.order("userA", 10000);
+        //ThreadA: B 사용자 20000원 주문
+        int userB = statefulService2.order("userB", 20000);
+
+        //ThreadA: 사용자A 주문 금액 조회
+//        int price = statefulService1.getPrice();
+        //내생각은 당연히 20000원이 나온다 메모리 구조를 생각해보면 간단한 원리..
+        System.out.println("price = " + userA);
+
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+
+}
