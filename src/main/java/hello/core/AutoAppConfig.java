7aae43d4f0b78b09d02b61d0bff7c233a3ef96fb
guddndhk <guddndhk@naver.com>
2022-03-13 22:48:41 +0900

    22/03/12 spring core study. no coding no sexy do it everything from ur self. ur time still on the load not gonna happen to stop @ComponentScan 과 @Autowired 에 대해 알아 보았다. @ComponentScan 를 사용하면 기존 AppConfig.java 에서 @Bean 으로 개발자가 직접 빈클래스를 지정하여 스프링 컨테이너에 적재 하였다면 @ComponentScan 사용하면 스프링 내부에서 알아서 빈을 찾아 등록해준다. @Autowired 역시 자동으로 의존성 주입을 해준다. 점점 개발자에게도 캡슐화를 적용하고 있는 프레임워크의 모습이다. 이처럼 프레임워크 특히나 스프링은 방대한 프레임 워크 이기때문에 기본 원리를 알지 못하면 개발자 조차도 캡슈화를 당하게 된다.. 항상 기본에 충실하며 암기가 아닌 프로세스의 원리와 구동에 대해서 이해하며 공부하자.

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+// 기존 정보 코드를 남기기 위해 excludeFilters 룰 사용함.
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
