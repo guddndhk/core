9bcb8d841de83c817fd0cc86aea185d6c96c6343
guddndhk <guddndhk@naver.com>
2022-03-17 19:29:16 +0900

    22/03/17 spring core study. no coding no sexy do it everything from ur self. ur time still on the load not gonna happen to stop 스프링 빈 충돌에 대해서..

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -13,4 +16,13 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    // 빈의 중복 상황 (수동, 자동) 등록시 수동이 우선권을 가짐
+    // 개발자가 의도를 가지고 이런 상황을 연출 한다면 좋겠지만 대부분의 개발상은 의도적으로 만든 경우보다는 여러 설정이 꼬여서
+    // 만들어지는 경우가 대부분이다 그렇기에 최근의 스트링 부트에서는 수동 및 자동 빈 등록시 충돌이 생긴다면 오류가 발생하도록
+    // 기본값을 바꾸었다 그 설정을 application.properties 에서 디폴트 값을 변경 해줄수 있다.
+    // 어설픈 추상화 보다는 명확성을 추구하자.
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
