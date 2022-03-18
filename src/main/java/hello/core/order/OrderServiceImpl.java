162416d4497f470d8b21335b5bfcbfdbde7bb7da
guddndhk <guddndhk@naver.com>
2022-03-18 23:41:13 +0900

    22/03/17 spring core study. no coding no sexy do it everything from ur self. ur time still on the load not gonna happen to stop 좀더 다양한 의존관게 주입 방법에 대하여..

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -15,6 +15,28 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    // setter 주입
+    // 스프링 라이플 사이클 상 스프링 컨테이너 빈 스캔후 Autowired 를 보고 해당 셋터를 주입한다.
+    // 하지만 웃긴건 생성자 주입만 빈 등록과 동시에 주입도 한다.. 애초에 해당 객체를 사용하면 생성자를 생성해야 하니까.
+    // 또한 선택적 의존관계 주입 가능 (required = false) 로 두면됨.
+
+/*    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+ */
+
+//    public OrderServiceImpl() {
+//
+//    }
+
+    // 생성자가 하나일 경우 @Autowired 생략이 가능하다.
+    // 하지만 나는 가독성을 위해 적어주는 편 이다. 현업에서는 안쓰는경우도 많아 지는 추세.
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
